347c504c45b458b7567962b998aa07144e251b3f
Matt O'Leary <mattosurf@gmail.com>
2022-01-30 13:13:48 -0800

    enable apple pay

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("_src/css/");
     eleventyConfig.addPassthroughCopy("_src/img/");
     eleventyConfig.addPassthroughCopy("_src/js/");
+    eleventyConfig.addPassthroughCopy("_src/.well-known/");
     
       return {
         dir: {
