174381824e60a0d841d393bd789c243eb4d5426e
Matt O'Leary <mattosurf@gmail.com>
2022-02-03 23:57:18 -0800

    favicons

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,12 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("_src/img/");
     eleventyConfig.addPassthroughCopy("_src/js/");
     eleventyConfig.addPassthroughCopy("_src/.well-known/");
+    eleventyConfig.addPassthroughCopy("_src/android-chrome-192x192.png");
+    eleventyConfig.addPassthroughCopy("_src/apple-touch-icon.png");
+    eleventyConfig.addPassthroughCopy("_src/favicon-16x16.png");
+    eleventyConfig.addPassthroughCopy("_src/favicon-32x32.png");
+    eleventyConfig.addPassthroughCopy("_src/favicon.ico");
+    eleventyConfig.addPassthroughCopy("_src/site.webmanifest");
     
       return {
         dir: {
